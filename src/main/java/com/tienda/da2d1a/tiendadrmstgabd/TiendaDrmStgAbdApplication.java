db4dfee43d6e70f0eb04e97eef35168b8f91222e
Daniel Ruiz Magro <145768755+daniielrm05@users.noreply.github.com>
2026-04-13 13:07:34 +0200

    Initial commit

---
+++ src/main/java/com/tienda/da2d1a/tiendadrmstgabd/TiendaDrmStgAbdApplication.java
@@ -0,0 +1,13 @@
+package com.tienda.da2d1a.tiendadrmstgabd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaDrmStgAbdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaDrmStgAbdApplication.class, args);
+    }
+
+}
