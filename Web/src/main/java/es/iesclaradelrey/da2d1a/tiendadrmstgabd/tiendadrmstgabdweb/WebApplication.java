670a80dd4ea8b4aad1a8fac989ebb5ec41810c43
Andriy_Borukh <ch1dryx@gmail.com>
2026-04-14 10:15:42 +0200

    Creado estructura inicial del proyecto (modulos)

---
+++ Web/src/main/java/es/iesclaradelrey/da2d1a/tiendadrmstgabd/tiendadrmstgabdweb/WebApplication.java
@@ -0,0 +1,13 @@
+package es.iesclaradelrey.da2d1a.tiendadrmstgabd.tiendadrmstgabdweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+}
---
+++ tienda-drm-stg-abd-common/src/main/java/es/iesclaradelrey/da2d1a/tiendadrmstgadb/Main.java
@@ -0,0 +1,7 @@
+package es.iesclaradelrey.da2d1a.tiendadrmstgadb;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
