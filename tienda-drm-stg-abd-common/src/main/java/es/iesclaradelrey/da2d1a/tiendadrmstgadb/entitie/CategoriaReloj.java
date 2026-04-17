40456c0f01a9385c59a07f72de9d1f3627b05bf6
Daniel Ruiz Magro <145768755+daniielrm05@users.noreply.github.com>
2026-04-17 09:25:08 +0200

    CategoriaReloj

--- tienda-drm-stg-abd-common/src/main/java/es/iesclaradelrey/da2d1a/tiendadrmstgadb/entitie/CategoriaReloj.java
+++ tienda-drm-stg-abd-common/src/main/java/es/iesclaradelrey/da2d1a/tiendadrmstgadb/entitie/CategoriaReloj.java
@@ -5,17 +5,63 @@ import lombok.Getter;
 /**
  * Define las categorías principales de la tienda de relojes.
  */
-@Getter
-public enum CategoriaReloj {
-    ELEGANTE("Elegante y Formal"),
-    DEPORTE("Deportivo y Resistente"),
-    SMARTWATCH("Reloj Inteligente"),
-    CASUAL("Uso Diario"),
-    COLECCIONISTA("Edición Limitada");
 
-    private final String nombreVisible;
+public class CategoriaReloj {
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private String imagen;
 
-    CategoriaReloj(String nombreVisible) {
-        this.nombreVisible = nombreVisible;
+    // Constructor vacío
+    public CategoriaReloj() {}
+
+    // Constructor con parámetros (te servirá para el CommandLineRunner)
+    public CategoriaReloj(Long id, String nombre, String descripcion, String imagen) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    @Override
+    public String toString() {
+        return "CategoriaReloj{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", imagen='" + imagen + '\'' +
+                '}';
     }
 }
