bafa48dd9d7704cc30309d229570397935e645d1
Daniel Ruiz Magro <145768755+daniielrm05@users.noreply.github.com>
2026-04-22 13:28:52 +0200

    Dani: Vistas de listado y detalle terminadas, DataLoader configurado y ultimos retoques

--- Web/src/main/java/es/iesclaradelrey/da2d1a/tiendadrmstgabd/tiendadrmstgabdweb/controllers/TestController.java
+++ Web/src/main/java/es/iesclaradelrey/da2d1a/tiendadrmstgabd/tiendadrmstgabdweb/controllers/TestController.java
@@ -1,11 +1,16 @@
 package es.iesclaradelrey.da2d1a.tiendadrmstgabd.tiendadrmstgabdweb.controllers;
 
+import es.iesclaradelrey.da2d1a.tiendadrmstgabd.entitie.CategoriaReloj;
 import es.iesclaradelrey.da2d1a.tiendadrmstgabd.service.CategoriaRelojService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/categorias")
@@ -14,8 +19,22 @@ public class TestController {
     private CategoriaRelojService service;
 
     @GetMapping
-    public String testList(Model model) {
+    public String testList(HttpServletRequest request, Model model) {
         model.addAttribute("categorias", service.buscarTodo());
+        model.addAttribute("requestURI", request.getRequestURI());
         return "categorias/listado";
+    }
+
+    @GetMapping("/{id}")
+    public String detalle(@PathVariable Long id, HttpServletRequest request, Model model) {
+        Optional<CategoriaReloj> cat = service.buscarPorId(id);
+
+        if (cat.isPresent()) {
+            model.addAttribute("categoria", cat.get());
+            model.addAttribute("requestURI", request.getRequestURI());
+            return "categorias/detalle";
+        }
+
+        return "redirect:/categorias"; // Si el ID no existe, vuelve atrás
     }
 }
