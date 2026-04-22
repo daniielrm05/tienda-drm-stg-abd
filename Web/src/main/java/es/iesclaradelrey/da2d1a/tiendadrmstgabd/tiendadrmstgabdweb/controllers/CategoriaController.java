69c9136bdd393cb55ac74b747bc1708ad7bcdfc1
Daniel Ruiz Magro <145768755+daniielrm05@users.noreply.github.com>
2026-04-22 13:53:38 +0200

    Dani: corregidas nombres imagenes en el index, limpiar y agregar comentarios

--- Web/src/main/java/es/iesclaradelrey/da2d1a/tiendadrmstgabd/tiendadrmstgabdweb/controllers/CategoriaController.java
+++ Web/src/main/java/es/iesclaradelrey/da2d1a/tiendadrmstgabd/tiendadrmstgabdweb/controllers/CategoriaController.java
@@ -14,24 +14,26 @@ import java.util.Optional;
 
 @Controller
 @RequestMapping("/categorias")
-public class TestController {
+public class CategoriaController {
     @Autowired
     private CategoriaRelojService service;
 
+    //listado de categorias
     @GetMapping
-    public String testList(HttpServletRequest request, Model model) {
-        model.addAttribute("categorias", service.buscarTodo());
-        model.addAttribute("requestURI", request.getRequestURI());
+    public String listado(HttpServletRequest request, Model model) {
+        model.addAttribute("categorias", service.buscarTodo()); //mete todas las categorias en el modelo
+        model.addAttribute("requestURI", request.getRequestURI()); //para que el menú de navegación (Navbar) saber en qué página estás e iluminar el botón correspondiente
         return "categorias/listado";
     }
 
+    //detalle de la categoria
     @GetMapping("/{id}")
     public String detalle(@PathVariable Long id, HttpServletRequest request, Model model) {
-        Optional<CategoriaReloj> cat = service.buscarPorId(id);
+        Optional<CategoriaReloj> cat = service.buscarPorId(id);  //Obtiene la categoria del id
 
-        if (cat.isPresent()) {
-            model.addAttribute("categoria", cat.get());
-            model.addAttribute("requestURI", request.getRequestURI());
+        if (cat.isPresent()) { //comprueba que exista esa categoria
+            model.addAttribute("categoria", cat.get()); //mete la categoria del id en el modelo
+            model.addAttribute("requestURI", request.getRequestURI()); //para que el menú de navegación (Navbar) saber en qué página estás e iluminar el botón correspondiente
             return "categorias/detalle";
         }
 
