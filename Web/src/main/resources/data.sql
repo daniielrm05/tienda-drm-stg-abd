eebe230cf5c2a1ebbd6b1856dccf2f67681c765b
SalvadorTrincado <SalvadorTrincado@users.noreply.github.com>
2026-04-29 23:18:50 +0200

    application.properties H2+JPA, dependencia JPA en pom.xml y data.sql (entrega 4)

---
+++ Web/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO categoria_reloj (nombre, descripcion, imagen) VALUES ('Elegante', 'Relojes para ocasiones formales', 'elegante.jpg');
+INSERT INTO categoria_reloj (nombre, descripcion, imagen) VALUES ('Deportivo', 'Resistentes al agua y con cronómetro', 'deportivo.jpg');
+INSERT INTO categoria_reloj (nombre, descripcion, imagen) VALUES ('Smartwatch', 'Lo último en tecnología y salud', 'smartwatch.jpg');
+INSERT INTO categoria_reloj (nombre, descripcion, imagen) VALUES ('Casual', 'Para el día a día, cómodos y versátiles', 'casual.jpg');
+INSERT INTO categoria_reloj (nombre, descripcion, imagen) VALUES ('Coleccionista', 'Ediciones limitadas y vintage', null);
