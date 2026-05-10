3489010bc6dd4947691646b8d2ef065f8d9be2c3
Daniel Ruiz Magro <145768755+daniielrm05@users.noreply.github.com>
2026-05-10 23:41:19 +0200

    Mis cambios antes del merge

--- common/src/main/java/es/iesclaradelrey/da2d1a/tiendadrmstgabd/repositories/EventoSeguridadRepository.java
+++ common/src/main/java/es/iesclaradelrey/da2d1a/tiendadrmstgabd/repositories/EventoSeguridadRepository.java
@@ -2,6 +2,8 @@ package es.iesclaradelrey.da2d1a.tiendadrmstgabd.repositories;
 
 import es.iesclaradelrey.da2d1a.tiendadrmstgabd.entities.EventoSeguridad;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EventoSeguridadRepository extends JpaRepository<EventoSeguridad, Long> {
 }
--- web/src/main/java/es/iesclaradelrey/da2d1a/tiendadrmstgabd/tiendadrmstgabdweb/WebApplication.java
+++ web/src/main/java/es/iesclaradelrey/da2d1a/tiendadrmstgabd/tiendadrmstgabdweb/WebApplication.java
@@ -7,9 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "es.iesclaradelrey.da2d1a")
 @EnableJpaRepositories(basePackages = "es.iesclaradelrey.da2d1a.tiendadrmstgabd.repositories")
-
 @EntityScan(basePackages = "es.iesclaradelrey.da2d1a.tiendadrmstgabd.entities")
-
 public class WebApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
