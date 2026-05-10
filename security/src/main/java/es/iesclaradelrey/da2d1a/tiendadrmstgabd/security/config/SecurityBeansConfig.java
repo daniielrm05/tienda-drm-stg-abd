d779e2c72e3566803b7827721221c4a356ddc3a4
Andriy_Borukh <ch1dryx@gmail.com>
2026-05-10 22:38:53 +0200

    Arreglado unos cuantos errores

--- security/src/main/java/es/iesclaradelrey/da2d1a/tiendadrmstgabd/security/config/SecurityBeansConfig.java
+++ security/src/main/java/es/iesclaradelrey/da2d1a/tiendadrmstgabd/security/config/SecurityBeansConfig.java
@@ -3,8 +3,10 @@ package es.iesclaradelrey.da2d1a.tiendadrmstgabd.security.config;
 import es.iesclaradelrey.da2d1a.tiendadrmstgabd.security.service.UsuarioDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -28,6 +30,12 @@ public class SecurityBeansConfig {
         authProvider.setPasswordEncoder(passwordEncoder);
 
         return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+    throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 }
